54d9788cb68813bec149134ead6100e7bc979533
Paulo Vitor <paulovitor.ssantos@outlook.com>
2024-11-19 20:42:20 +0000

    Initial customization

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,27 +1,27 @@
 "use client";
-import { Github, Mail, Linkedin } from "lucide-react";
+import { Github, Mail, Twitter } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
 const socials = [
 	{
-		icon: <Linkedin size={20} />,
-		href: "https://www.linkedin.com/in/paulo-ssantos/",
-		label: "Linkedin",
-		handle: "@paulo-ssantos",
+		icon: <Twitter size={20} />,
+		href: "https://twitter.com/chronark_",
+		label: "Twitter",
+		handle: "@chronark_",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:paulo.ssantos@updeploy.tech",
+		href: "mailto:dev@chronark.com",
 		label: "Email",
-		handle: "paulo.ssantos@updeploy.tech",
+		handle: "dev@chronark.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/paulo-ssantos",
+		href: "https://github.com/chronark",
 		label: "Github",
-		handle: "paulo-ssantos",
+		handle: "chronark",
 	},
 ];
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,23 @@
-import "../global.css";
 import { Inter } from "@next/font/google";
 import LocalFont from "@next/font/local";
 import { Metadata } from "next";
+import "../global.css";
 import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "Paulo Santos | Personal Website",
+    template: "%s | Paulo Santos",
   },
-  description: "Co-founder of unkey.dev and founder of planetfall.io",
+  description: "Personal website and portfolio of Paulo Santos",
   openGraph: {
-    title: "chronark.com",
-    description:
-      "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+    title: "Paulo Santos | Personal Website",
+    description: "Personal website and portfolio of Paulo Santos",
+    url: "https://paulo-ssantos.updeploy.tech",
+    siteName: "Paulo Santos | Personal Website",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://paulo-ssantos.updeploy.tech/og.png",
         width: 1920,
         height: 1080,
       },
@@ -38,7 +37,7 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: "Chronark",
+    title: "Paulo Santos | Personal Website",
     card: "summary_large_image",
   },
   icons: {
